16b9bee1e994389a277b62e512e9f712a4a32e69
김성민 <floor3th@gmail.com>
2025-05-15 21:42:56 +0900

    lint 설정중

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,9 +2,7 @@
  * @type {import('lint-staged').Configuration}
  */
 const config = {
-  '*.{js,jsx,ts,tsx,json,css,md,html}': [
-    'pnpm run prettier',
-  ],
+  '*.{js,jsx,ts,tsx,json,css,md,html}': ['pnpm run prettier'],
 };
 
 export default config;
