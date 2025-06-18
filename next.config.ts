a505992f8a1b9517a72e907a43d9233588e1a7a8
ksm7973 <floor3th@gmail.com>
2025-06-18 11:49:00 +0900

    allowedDevOrigins 추가

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,13 @@ const nextConfig: NextConfig = {
   },
 
   productionBrowserSourceMaps: false,
+
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'http://192.168.11.116:3000',
+    'https://192.168.11.116:3000',
+  ],
 };
 
 export default nextConfig;
